2149aa0021e6a366bddbab0f0db125b47d7f2d20
jonass-hr <aquiles.wf@gmail.com>
2026-03-31 20:49:18 +0200

    Refactor Review class to include getter and setter methods

--- backend/bookify_project/src/main/java/es/code_urjc_g5/bookify_project/models/Review.java
+++ backend/bookify_project/src/main/java/es/code_urjc_g5/bookify_project/models/Review.java
@@ -11,7 +11,33 @@ public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
     private String review_text;
     private int review_rating;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReview_text() {
+        return review_text;
+    }
+
+    public void setReview_text(String review_text) {
+        this.review_text = review_text;
+    }
+
+    public int getReview_rating() {
+        return review_rating;
+    }
+
+    public void setReview_rating(int review_rating) {
+        this.review_rating = review_rating;
+    }
+
 
 }
