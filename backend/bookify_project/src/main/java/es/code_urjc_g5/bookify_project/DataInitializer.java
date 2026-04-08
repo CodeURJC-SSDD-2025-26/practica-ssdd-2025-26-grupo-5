6cd39e24e046b798919af7630b21bcace6cea282
jonass-hr <aquiles.wf@gmail.com>
2026-04-08 20:33:38 +0200

    Refactor Book model to use Lombok's Builder pattern and update DataInitializer for cleaner object creation; modify application properties for Hibernate settings

--- backend/bookify_project/src/main/java/es/code_urjc_g5/bookify_project/DataInitializer.java
+++ backend/bookify_project/src/main/java/es/code_urjc_g5/bookify_project/DataInitializer.java
@@ -14,20 +14,23 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        Book book = new Book();
-        book.setTitle("El Hobbit");
-        book.setAuthor("J.R.R. Tolkien");
-        book.setGenre("Fantasía");
-        book.setSynopsis("Escrito para los hijos de J.R.R. Tolkien, El Hobbit alcanzó un éxito inmediato cuando se publicó en 1937. Bilbo Bolsón, un hobbit que disfruta de su cómoda vida, se ve arrastrado a una aventura inesperada por el mago Gandalf y un grupo de trece enanos liderados por Thorin Escudo de Roble.\n" + 
+        bookRepository.deleteAll(); //Borra todo aliniciar la aplicacion para evitar duplicados
+        
+        //Book builder para construir los modelos mediante notacion lombok
+        Book book = Book.builder()
+                .title("El Hobbit")
+                .author("J.R.R. Tolkien")
+                .genre("Fantasía")
+                .synopsis("Escrito para los hijos de J.R.R. Tolkien, El Hobbit alcanzó un éxito inmediato cuando se publicó en 1937. Bilbo Bolsón, un hobbit que disfruta de su cómoda vida, se ve arrastrado a una aventura inesperada por el mago Gandalf y un grupo de trece enanos liderados por Thorin Escudo de Roble.\n" + 
                         "\n" + 
-                        "Su objetivo es recuperar el tesoro custodiado por el temible dragón Smaug en la Montaña Solitaria. A través de este viaje épico, Bilbo abandonará la seguridad de su hogar para enfrentarse a trolls, orcos y acertijos en la oscuridad. En las profundidades de la tierra, encontrará no solo un valor que no sabía que poseía, sino también un anillo mágico cuyo origen y poder cambiarán el destino de la Tierra Media para siempre.");
-        book.setCoverUrl("https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1546071216i/5907.jpg");
-        book.setPages(310);
-        book.setLanguage("Español");
-        book.setPublicationYear(1937);
-        book.setScore(0);
-        book.setReviewCount(0);
-
+                        "Su objetivo es recuperar el tesoro custodiado por el temible dragón Smaug en la Montaña Solitaria. A través de este viaje épico, Bilbo abandonará la seguridad de su hogar para enfrentarse a trolls, orcos y acertijos en la oscuridad. En las profundidades de la tierra, encontrará no solo un valor que no sabía que poseía, sino también un anillo mágico cuyo origen y poder cambiarán el destino de la Tierra Media para siempre.")
+                .coverUrl("https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1546071216i/5907.jpg")
+                .pages(310)
+                .language("Español")
+                .publicationYear(1937)
+                .score(0)
+                .reviewCount(0)
+                .build();
         bookRepository.save(book);
     }
 }
--- backend/bookify_project/src/main/java/es/code_urjc_g5/bookify_project/models/Book.java
+++ backend/bookify_project/src/main/java/es/code_urjc_g5/bookify_project/models/Book.java
@@ -7,13 +7,20 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
-@Setter
-@Getter
 @Entity
+@Data
 @Table(name = "books")
+@Builder //Para usar el patrón builder en la clase Book, lo que facilita la creación de objetos Book con una sintaxis más fluida y legible. Esto es especialmente útil cuando se tienen muchos atributos, ya que permite establecer solo los necesarios sin tener que usar un constructor con muchos parámetros.
+@NoArgsConstructor
+@AllArgsConstructor //Necesario para usar el patrón builder, ya que proporciona un constructor con todos los argumentos que el builder necesita para crear una instancia de Book.
 public class Book {
+
+    
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
