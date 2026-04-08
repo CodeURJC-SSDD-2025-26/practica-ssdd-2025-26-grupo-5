57224750dd490429b045321ded96d041dfacd28c
Mengying04 <m.xia.2023@alumnos.urjc.es>
2026-04-08 17:02:56 +0200

    Remove unused admin route from headerController

--- bookify_project/src/main/java/es/code_urjc_g5/bookify_project/controllers/headerController.java
+++ bookify_project/src/main/java/es/code_urjc_g5/bookify_project/controllers/headerController.java
@@ -5,12 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class headerController {
-    
-    @GetMapping("/admin")
-    public String aboutUs() {
-        return "admin";
-    }
-
     @GetMapping("/profile")
     public String privacyPolicy() {
         return "profile";
