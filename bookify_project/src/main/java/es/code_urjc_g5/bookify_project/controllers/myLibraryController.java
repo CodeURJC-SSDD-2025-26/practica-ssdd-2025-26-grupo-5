69ce7e4a3a9a0fd2f907f6a36de862540c91b307
jonass-hr <aquiles.wf@gmail.com>
2026-04-09 23:26:11 +0200

    Refactor myLibraryController to include user authentication and collections in the myLibrary view

--- bookify_project/src/main/java/es/code_urjc_g5/bookify_project/controllers/myLibraryController.java
+++ bookify_project/src/main/java/es/code_urjc_g5/bookify_project/controllers/myLibraryController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
+import es.code_urjc_g5.bookify_project.services.UserService;
 
 @Controller
 public class myLibraryController {
@@ -16,13 +18,16 @@ public class myLibraryController {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/myLibrary")
-    public String myLibrary(Model model) {
+    public String myLibrary(Model model, Authentication authentication) {
 
-        List<Book> books = bookRepository.findAll();
-
-        model.addAttribute("Books", books);
-        model.addAttribute("bookCount", books.size());
+        userService.findByEmail(authentication.getName()).ifPresent(user -> {
+            model.addAttribute("user", user);
+            model.addAttribute("collections", user.getCollections());
+        });
 
         return "myLibrary";
     }
