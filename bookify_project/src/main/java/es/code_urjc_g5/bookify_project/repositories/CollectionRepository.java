90617cebdfd1e02903ffef176a70a322c3e4ebb7
jonass-hr <aquiles.wf@gmail.com>
2026-04-12 14:41:01 +0200

    Add method to find collections by user in CollectionRepository

--- bookify_project/src/main/java/es/code_urjc_g5/bookify_project/repositories/CollectionRepository.java
+++ bookify_project/src/main/java/es/code_urjc_g5/bookify_project/repositories/CollectionRepository.java
@@ -3,9 +3,10 @@ package es.code_urjc_g5.bookify_project.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.code_urjc_g5.bookify_project.models.Collection;
-
+import es.code_urjc_g5.bookify_project.models.User;
+import java.util.List;
 
 public interface CollectionRepository extends JpaRepository<Collection, Long> {
 
-    
+    List<Collection> findByUser(User user);
 }
