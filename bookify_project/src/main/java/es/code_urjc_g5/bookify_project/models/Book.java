565b0d5a19740b0aa1fe5275f7174669e00129f7
jonass-hr <aquiles.wf@gmail.com>
2026-04-12 15:05:04 +0200

    Removed lombok @Data annotations due to several functionality problems in myLibrary,

--- bookify_project/src/main/java/es/code_urjc_g5/bookify_project/models/Book.java
+++ bookify_project/src/main/java/es/code_urjc_g5/bookify_project/models/Book.java
@@ -15,11 +15,15 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Entity
-@Data
 @Table(name = "books")
-@Builder //Para usar el patrón builder en la clase Book, lo que facilita la creación de objetos Book con una sintaxis más fluida y legible. Esto es especialmente útil cuando se tienen muchos atributos, ya que permite establecer solo los necesarios sin tener que usar un constructor con muchos parámetros.
+@Builder // Para usar el patrón builder en la clase Book, lo que facilita la creación de
+         // objetos Book con una sintaxis más fluida y legible. Esto es especialmente
+         // útil cuando se tienen muchos atributos, ya que permite establecer solo los
+         // necesarios sin tener que usar un constructor con muchos parámetros.
 @NoArgsConstructor
-@AllArgsConstructor //Necesario para usar el patrón builder, ya que proporciona un constructor con todos los argumentos que el builder necesita para crear una instancia de Book.
+@AllArgsConstructor // Necesario para usar el patrón builder, ya que proporciona un constructor con
+                    // todos los argumentos que el builder necesita para crear una instancia de
+                    // Book.
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -38,8 +42,121 @@ public class Book {
     private int reviewCount;
     @jakarta.persistence.Lob
     @jakarta.persistence.Column(columnDefinition = "LONGBLOB")
-    private byte[] pdfFile;  
-    
+    private byte[] pdfFile;
+
     @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Review> reviews;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public Long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Long isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getCoverUrl() {
+        return coverUrl;
+    }
+
+    public void setCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public int getReviewCount() {
+        return reviewCount;
+    }
+
+    public void setReviewCount(int reviewCount) {
+        this.reviewCount = reviewCount;
+    }
+
+    public byte[] getPdfFile() {
+        return pdfFile;
+    }
+
+    public void setPdfFile(byte[] pdfFile) {
+        this.pdfFile = pdfFile;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
 }
